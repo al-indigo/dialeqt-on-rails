3cb811c7751c0b6e0b0efbf7cf3130a3deeaa41f
Oleg Borisenko <al@somestuff.ru>
2014-06-01 22:57:33 +0400

    sound attachment controller changed in paradigms

--- app/models/paradigm_sound.rb
+++ app/models/paradigm_sound.rb
@@ -1,4 +1,7 @@
 class ParadigmSound < ActiveRecord::Base
-  has_attached_file(:sound)
+  has_attached_file(:sound,
+                    :styles => {:wav => {:format => "wav" },
+                                :mp3 => {:format => "mp3" } },
+                    :path => ":rails_root/public/system/paradigm_sounds/:id/:style/:filename" )
   belongs_to(:paradigm)
 end
